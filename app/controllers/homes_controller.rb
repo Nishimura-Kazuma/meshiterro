4812bb4ea07cec03b4efa3a16100d7065b5ea8de
EC2 Default User <ec2-user@ip-172-31-9-18.ap-northeast-1.compute.internal>
2023-01-18 04:31:32 +0000

    [Finish] 4-7

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,4 +3,9 @@ class HomesController < ApplicationController
   def top
     
   end
+  
+  def about
+    
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,16 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
+  get 'homes/about' => 'homes#about' , as:'about'
 end
+
+
+# 例文
+# resources :lists
+  # get 'lists/new'
+  # post 'lists' => 'lists#create'
+  # get 'lists' => 'lists#index'
+  # get 'lists/:id' => 'lists#show', as: 'list'
+  # get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+  # patch 'lists/:id' => 'lists#update', as: 'update_list'
+  # delete 'lists/:id' => 'lists#destroy', as: 'destroy_list'
