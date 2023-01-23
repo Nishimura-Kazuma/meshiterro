fea95690bb6e4f05bfb43c3fa7315d6f1df14d75
EC2 Default User <ec2-user@ip-172-31-9-18.ap-northeast-1.compute.internal>
2023-01-23 07:29:18 +0000

    [Finish] 4-19

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -2,6 +2,7 @@ class PostImage < ApplicationRecord
   has_one_attached :image
   belongs_to :user
   has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   
   def get_image
     unless image.attached?
@@ -10,6 +11,11 @@ class PostImage < ApplicationRecord
     end
     image
   end
+  
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   # devise :database_authenticatable, :registerable,
